3c1320f6078d195964d45191c8b97000bc1f3ff1
adisbladis <adisbladis@gmail.com>
2023-10-27 16:01:44 +1300

    templates: Add comments to requirements.txt template

--- templates/requirements-txt/flake.nix
+++ templates/requirements-txt/flake.nix
@@ -15,6 +15,7 @@
     let
       pyproject = import (pyproject-nix + "/lib") { inherit (nixpkgs) lib; };
 
+      # Load/parse requirements.txt
       project = pyproject.project.loadRequirementsTxt {
         requirements = ./requirements.txt;
       };
@@ -25,11 +26,15 @@
       pkgs = nixpkgs.legacyPackages.${system};
       python = pkgs.python3;
 
-      pythonEnv = assert pyproject.validators.validateVersionConstraints { inherit project python; } == { }; (
-        pkgs.python3.withPackages (pyproject.renderers.withPackages {
-          inherit project python;
-        })
-      );
+      pythonEnv =
+        # Assert that versions from nixpkgs matches what's described in requirements.txt
+        # In projects that are overly strict about pinning it might be best to remove this assertion entirely.
+        assert pyproject.validators.validateVersionConstraints { inherit project python; } == { }; (
+          # Render requirements.txt into a Python withPackages environment
+          pkgs.python3.withPackages (pyproject.renderers.withPackages {
+            inherit project python;
+          })
+        );
 
     in
     {
