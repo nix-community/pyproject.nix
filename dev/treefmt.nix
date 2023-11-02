1961072417d7a20a8eae2fb938ef4445f6332b41
adisbladis <adisbladis@gmail.com>
2023-11-02 13:51:46 +1300

    treefmt: refactor

--- dev/treefmt.nix
+++ dev/treefmt.nix
@@ -1,46 +1,20 @@
-{ pkgs, ... }: {
+_: {
   # Used to find the project root
   projectRootFile = "flake.lock";
 
+  # JS/doc
   programs.prettier.enable = true;
+  settings.formatter.prettier.excludes = [ "highlight.js" ];
 
+  # Shell
   programs.shellcheck.enable = true;
 
-  settings.formatter = {
-    nix = {
-      command = "sh";
-      options = [
-        "-eucx"
-        ''
-          ${pkgs.lib.getExe pkgs.deadnix} --edit "$@"
+  # Nix
+  programs.deadnix.enable = true;
+  programs.statix.enable = true;
+  programs.nixpkgs-fmt.enable = true;
 
-          for i in "$@"; do
-            ${pkgs.lib.getExe pkgs.statix} fix "$i"
-          done
-
-          ${pkgs.lib.getExe pkgs.nixpkgs-fmt} "$@"
-        ''
-        "--"
-      ];
-      includes = [ "*.nix" ];
-      excludes = [ ];
-    };
-
-    prettier = {
-      excludes = [ "highlight.js" ];
-    };
-
-    python = {
-      command = "sh";
-      options = [
-        "-eucx"
-        ''
-          ${pkgs.lib.getExe pkgs.python3.pkgs.black} "$@"
-          ${pkgs.lib.getExe pkgs.ruff} --fix "$@"
-        ''
-        "--" # this argument is ignored by bash
-      ];
-      includes = [ "*.py" ];
-    };
-  };
+  # Python
+  programs.black.enable = true;
+  programs.ruff.enable = true;
 }
