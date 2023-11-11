f7ce0e48c2379954f7903190a485c616bf5271e6
adisbladis <adisbladis@gmail.com>
2023-11-11 22:27:00 +1300

    templates: use project accessors for validators & renderers

--- templates/requirements-txt/flake.nix
+++ templates/requirements-txt/flake.nix
@@ -2,10 +2,8 @@
   description = "Construct development shell from requirements.txt";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.pyproject-nix.url = "github:nix-community/pyproject.nix";
-  inputs.pyproject-nix.flake = false; # Don't use the pyproject.nix flake directly to avoid its inputs in our closure
 
   outputs =
     { nixpkgs
@@ -14,36 +12,31 @@
     , ...
     }:
     let
-      pyproject = import (pyproject-nix + "/lib") { inherit (nixpkgs) lib; };
-
       # Load/parse requirements.txt
-      project = pyproject.project.loadRequirementsTxt {
-        requirements = ./requirements.txt;
+      project = pyproject-nix.lib.project.loadRequirementsTxt {
+        projectRoot = ./.;
       };
 
-    in
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
       python = pkgs.python3;
 
       pythonEnv =
         # Assert that versions from nixpkgs matches what's described in requirements.txt
         # In projects that are overly strict about pinning it might be best to remove this assertion entirely.
-        assert pyproject.validators.validateVersionConstraints { inherit project python; } == { }; (
+        assert project.validators.validateVersionConstraints { inherit python; } == { }; (
           # Render requirements.txt into a Python withPackages environment
-          pkgs.python3.withPackages (pyproject.renderers.withPackages {
-            inherit project python;
+          pkgs.python3.withPackages (project.renderers.withPackages {
+            inherit python;
           })
         );
 
     in
     {
-      devShell =
+      devShells.x86_64-linux.default =
         pkgs.mkShell {
           packages = [
             pythonEnv
           ];
         };
-    });
+    };
 }
