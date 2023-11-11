1db4afd5b8aa550632289cb827dcb3e904778101
adisbladis <adisbladis@gmail.com>
2023-11-11 18:13:07 +1300

    flake template: Use `project.renderers` & reword comments

--- templates/pyproject/flake.nix
+++ templates/pyproject/flake.nix
@@ -1,40 +1,50 @@
 {
   description = "A basic flake using pyproject.toml project metadata";
 
-  inputs.pyproject.url = "github:nix-community/pyproject.nix";
-  inputs.pyproject.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.pyproject-nix.url = "github:nix-community/pyproject.nix";
+  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { nixpkgs, pyproject, ... }:
+  outputs = { nixpkgs, pyproject-nix, ... }:
     let
       inherit (nixpkgs) lib;
 
       # Loads pyproject.toml into a high-level project representation
       # Do you notice how this is not tied to any `system` attribute or package sets?
       # That is because `project` refers to a pure data representation.
-      project = pyproject.lib.project.loadPyproject {
-        # Read & unmarshal pyproject.toml
-        pyproject = lib.importTOML ./pyproject.toml;
+      project = pyproject-nix.lib.project.loadPyproject {
+        # Read & unmarshal pyproject.toml relative to this project root.
+        # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
+        projectRoot = ./.;
       };
 
       # This example is only using x86_64-linux
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-      # We are using the default Python3 interpreter & package set
+      # We are using the default Python3 interpreter & package set.
+      # This means that you are purposefully ignoring:
+      # - Version bounds
+      # - Dependency sources (meaning local path dependencies won't resolve to the local path)
+      #
+      # To use packages from local sources see "Overriding Python packages" in the nixpkgs manual:
+      # https://nixos.org/manual/nixpkgs/stable/#reference
+      #
+      # Or use an overlay generator such as pdm2nix:
+      # https://github.com/adisbladis/pdm2nix
       python = pkgs.python3;
 
     in
     {
-
       # Create a development shell containing dependencies from `pyproject.toml`
       devShells.x86_64-linux.default =
         let
           # Returns a function that can be passed to `python.withPackages`
-          arg = pyproject.lib.renderers.withPackages { inherit python project; };
+          arg = project.renderers.withPackages { inherit python; };
 
           # Returns a wrapped environment (virtualenv like) with all our packages
           pythonEnv = python.withPackages arg;
 
         in
+        # Create a devShell like normal.
         pkgs.mkShell {
           packages = [ pythonEnv ];
         };
@@ -43,8 +53,10 @@
       packages.x86_64-linux.default =
         let
           # Returns an attribute set that can be passed to `buildPythonPackage`.
-          attrs = pyproject.lib.renderers.buildPythonPackage { inherit python project; };
+          attrs = project.renderers.buildPythonPackage { inherit python; };
         in
+        # Pass attributes to buildPythonPackage.
+        # Here is a good spot to add on any missing attributes such as `src`.
         python.pkgs.buildPythonPackage attrs;
     };
 }
