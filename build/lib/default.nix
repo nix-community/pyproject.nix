929fddf1afe8148643731fac43cb37eb1c9e14c5
adisbladis <adisbladis@gmail.com>
2024-09-24 00:24:06 +0000

    doc: Add build lib root

--- build/lib/default.nix
+++ build/lib/default.nix
@@ -6,17 +6,21 @@ in
 
 fix (
   self:
-  mapAttrs (_: path: import path ({ inherit pyproject-nix lib; } // self)) {
-    renderers = ./renderers.nix;
-    resolvers = ./resolvers.nix;
-  }
-  // {
+  {
+    /*
+      Check if a package is a bootstrap package by it's name.
 
+      This needs to be used by lockfile consumers to check if a package needs pyprojectBootstrapHook instead of pyprojectHook.
+    */
     isBootstrapPackage = flip elem [
       "flit-core"
       "pyproject-hooks"
       "packaging"
       "build"
     ];
+  }
+  // mapAttrs (_: path: import path ({ inherit pyproject-nix lib; } // self)) {
+    renderers = ./renderers.nix;
+    resolvers = ./resolvers.nix;
   }
 )
